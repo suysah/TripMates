f6c128fdb5f40b668d14dd9a8884f3034b137d31
Suyash Nagar <suyashnagar81@gmail.com>
2025-08-18 20:22:40 +0530

    if no user then will show login page

--- Client/src/components/PrivateRoute.jsx
+++ Client/src/components/PrivateRoute.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router-dom";
 import useAuth from "../context/useAuth";
+import Login from "../pages/Login";
+import Spinner from "./Spinner";
 
 const PrivateRoute = () => {
   const { user, loading } = useAuth();
 
   // console.log("User", user);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <p>
+        <Spinner />
+      </p>
+    );
 
-  return user ? <Outlet /> : null;
+  return user ? <Outlet /> : <Login />;
 };
 
 export default PrivateRoute;
